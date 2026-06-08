d815843333b21710b56d22214cd37c51472f92a9
Ashiga35 <ashijeba06@gmail.com>
2026-06-08 17:38:44 +0530

    Function basics

---
+++ function.java
@@ -0,0 +1,19 @@
+class function{
+    int evenorodd(int num){
+        if(num%2==0){
+            System.out.println("The number " + num + " is even");
+        }
+        else{
+            System.out.println("The number " + num + " is odd");
+        }
+        return num ;
+    }
+    public static void main(String args[]){
+       
+        function obj = new function();
+        int result =obj.evenorodd(4);
+        System.out.println(result);
+
+
+    }
+}
